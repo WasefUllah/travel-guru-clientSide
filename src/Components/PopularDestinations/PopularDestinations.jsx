e28cacce8fea6b2b37a1e6718609483ce39be638
Shahin <shahinalhasan2254@gmail.com>
2025-07-10 02:34:12 +0600

    added popular destinations

--- src/Components/PopularDestinations/PopularDestinations.jsx
+++ src/Components/PopularDestinations/PopularDestinations.jsx
@@ -13,7 +13,16 @@ const PopularDestinations = () => {
     .catch();
   return (
    <div>
-    
+     <div className="my-10">
+      <h1 className="text-primary text-2xl md:text-4xl lg:text-6xl font-bold my-4">
+        Popular Destinations
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+        {popular.map((dest) => (
+          <DestinationCard destination={dest}></DestinationCard>
+        ))}
+      </div>
+    </div>
    </div>
   );
 };
